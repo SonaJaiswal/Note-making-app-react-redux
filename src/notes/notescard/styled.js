4cdcc737ee3fee06fc53a9cb444fb9297bd107a5
SonaJaiswal <68175083+SonaJaiswal@users.noreply.github.com>
2022-07-01 13:28:27 +0530

    Create styled.js

---
+++ src/notes/notescard/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CardElement = styled.div`
+  border: 1px red;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px 48px;
+  width: fit-content;
+  border-radius: 8px;
+  margin: 16px;
+`;
